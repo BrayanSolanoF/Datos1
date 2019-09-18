495ee7a8c1b03d480f87fe2cfc2e2e7caa504a2e
Brayan <brayan.sf8@gmail.com>
2019-09-18 01:12:37 -0600

    Manipulacion de componentes

--- AvanceCircuitDesigner/src/Interfaz/Main.java
+++ AvanceCircuitDesigner/src/Interfaz/Main.java
@@ -1,5 +1,6 @@
 package Interfaz;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.Serializable;
@@ -28,9 +29,98 @@ public class Main   {
         splitPane.setTopComponent(buttonspanel);
         frame.add(splitPane, BorderLayout.EAST);
 
+        //Botones como Jbuttons patra implementarlos a JPanel
+        JButton andbutton = new JButton();
+        //Se busca funcion para utilizar los botones con imagenes de cada componente
+        try {
+            Image img = ImageIO.read(getClass().getResource("./and.png"));
+            andbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        JButton orbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./or.png"));
+            orbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton notbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./not.png"));
+            notbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton nandbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./nand.png"));
+            nandbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton norbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./nor.png"));
+            norbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton xorbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./xor.png"));
+            xorbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        JButton xnorbutton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./xnor.png"));
+            xnorbutton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton newConnectionButton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./Thread.png"));
+            newConnectionButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton startButton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./start.png"));
+            startButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton endButton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./end.png"));
+            endButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton simulateButton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./simulate.png"));
+            simulateButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        JButton eraseButton = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("./erase.png"));
+            eraseButton.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
         
         //Agregar botones a la paleta
-        /*buttonspanel.add(andbutton);
+        buttonspanel.add(andbutton);
         buttonspanel.add(orbutton);
         buttonspanel.add(notbutton);
         buttonspanel.add(nandbutton);
@@ -41,7 +131,7 @@ public class Main   {
         buttonspanel.add(startButton);
         buttonspanel.add(endButton);
         buttonspanel.add(simulateButton);
-        buttonspanel.add(eraseButton);*/
+        buttonspanel.add(eraseButton);
 
 
 
